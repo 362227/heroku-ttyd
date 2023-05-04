5424c31b775009fa96a750c7bc99af1be00555bc
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-05-04 20:06:37 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir ~/.screen && chmod 700 ~/.screen
 export SCREENDIR=$HOME/.screen
-/usr/bin/ttyd -p $PORT  -t 'theme={"background": "white", "foreground": "black","selection": "#ddb6fc"}'  -t fontSize=20  -t 'scrollback=2000' -t titleFixed=Colab_ttyd  tmux new -A -s ttyd
+/usr/bin/ttyd -p $PORT  -c 10362227:y362227 -t 'theme={"background": "white", "foreground": "black","selection": "#ddb6fc"}'  -t fontSize=20  -t 'scrollback=2000' -t titleFixed=Colab_ttyd  tmux new -A -s ttyd
